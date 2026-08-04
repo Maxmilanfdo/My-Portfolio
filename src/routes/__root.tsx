a3031cbdb99dd1e4afe320f90e2d50f9f4327d14
MaxmilanFdo <maxmilanfdo2003@gmail.com>
2026-08-04 17:54:13 +0530

    Added Google tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -117,6 +117,12 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-82PFCCKPTP"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-82PFCCKPTP');`,
+          }}
+        />
       </head>
       <body>
         {children}
